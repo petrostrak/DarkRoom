01c435e9067c73b34adf2f1d9585807193720370
petrostrak <pit.trak@gmail.com>
2021-01-05 15:35:27 +0200

    Creating the contact view

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,7 +22,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@darkroom.com\">support@darkroom.com</a>.")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		fmt.Fprint(w, err)
+	}
 }
 
 // func error(w http.ResponseWriter, r *http.Request) {
@@ -45,6 +50,10 @@ func handle404(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}
