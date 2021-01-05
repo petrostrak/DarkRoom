c5f2fbbb8a0316b7573d91abae499a8eb5149820
petrostrak <pit.trak@gmail.com>
2021-01-06 00:08:55 +0200

    Creating a Bootstrap layout

--- main.go
+++ main.go
@@ -15,14 +15,15 @@ var (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.Execute(w, nil); err != nil {
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	err := contactView.Template.Execute(w, nil)
+	err := contactView.Template.ExecuteTemplate(w, homeView.Layout, nil)
 	if err != nil {
 		fmt.Fprint(w, err)
 	}
@@ -49,8 +50,8 @@ func handle404(w http.ResponseWriter, r *http.Request) {
 
 // go get github.com/julienschmidt/httprouter
 func main() {
-	homeView = views.NewView("views/home.gohtml")
-	contactView = views.NewView("views/contact.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
--- views/view.go
+++ views/view.go
@@ -3,8 +3,11 @@ package views
 import "html/template"
 
 // NewView func to instantiate view objects
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files, 
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/footer.gohtml",
+	)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -12,10 +15,12 @@ func NewView(files ...string) *View {
 	}
 	return &View{
 		Template: t,
+		Layout: layout,
 	}
 }
 
 // the view structure
 type View struct {
 	Template *template.Template
+	Layout string
 }
