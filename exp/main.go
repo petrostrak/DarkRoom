b3b85803bbbd6e19b0a8d44b34e5936e4f0c14fb
petros <pit.trak@gmail.com>
2021-01-09 18:13:56 +0200

    Handling relational data with Go's sql package

--- exp/main.go
+++ exp/main.go
@@ -24,12 +24,12 @@ func main() {
 	}
 	defer db.Close()
 
-	type User struct {
-		id, age                    int
-		firstName, lastName, email string
-	}
+	// type User struct {
+	// 	id, age                    int
+	// 	firstName, lastName, email string
+	// }
 
-	var users []User
+	// var users []User
 	// var id, age int
 	// var firstName, lastName, email string
 
@@ -41,20 +41,34 @@ func main() {
 
 	// err = db.QueryRow(`
 	// SELECT id, first_name, email FROM users WHERE id=$1`, 3).Scan(&id, &name, &email)
+	_, err = db.Exec(`
+	SELECT * FROM users
+	INNER JOIN orders ON users.id=orders.user_id`)
+	// for i := 1; i <= 6; i++ {
+	// 	userID := 1
+	// 	if i > 3 {
+	// 		userID = 3
+	// 	}
+	// 	amount := i * 75
+	// 	description := fmt.Sprintf("USB-C Adapter x%d", i)
+	// _, err = db.Exec(`
+	// SELECT * FROM users
+	// INNER JOIN orders ON users.id=orders.user_id
+	// INSERT INTO orders(user_id, amount, description)
+	// VALUES($1,$2, $3)`, userID, amount, description)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
 
-	rows, err := db.Query(`
-	SELECT id, age, first_name, last_name, email FROM users WHERE age > $1`, 30)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var user User
-		if err = rows.Scan(&user.id, &user.age, &user.firstName, &user.lastName, &user.email); err != nil {
-			panic(err)
-		}
-		// fmt.Println("id:", id, "Age:", age, "First name:", firstName, "Last name:", lastName, "Email:", email)
-		users = append(users, user)
-		fmt.Println(users)
-	}
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var user User
+	// 	if err = rows.Scan(&user.id, &user.age, &user.firstName, &user.lastName, &user.email); err != nil {
+	// 		panic(err)
+	// 	}
+	// 	// fmt.Println("id:", id, "Age:", age, "First name:", firstName, "Last name:", lastName, "Email:", email)
+	// 	users = append(users, user)
+	// 	fmt.Println(users)
+	// }
 }
