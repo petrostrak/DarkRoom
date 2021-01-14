42c36186ab6acb9dc71c14c35a9672a5cbd55ca4
petros <pit.trak@gmail.com>
2021-01-14 16:53:05 +0200

    Creating the login form

--- controllers/users.go
+++ controllers/users.go
@@ -9,8 +9,9 @@ import (
 
 // The Users structure
 type Users struct {
-	NewView *views.View
-	us      *models.UserService
+	NewView   *views.View
+	LoginView *views.View
+	us        *models.UserService
 }
 
 // The SignupForm struct for signing up
@@ -26,8 +27,9 @@ type SignupForm struct {
 // initial setup.
 func NewUsers(us *models.UserService) *Users {
 	return &Users{
-		NewView: views.NewView("bootstrap", "users/new"),
-		us:      us,
+		NewView:   views.NewView("bootstrap", "users/new"),
+		LoginView: views.NewView("bootstrap", "users/login"),
+		us:        us,
 	}
 }
 
@@ -59,4 +61,22 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintln(w, user)
+}
+
+type LoginForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// is used to verify the provided email address and password and then
+// log the user in if they are correct
+//
+// POST /login
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	form := LoginForm{}
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	// do something with the login form
+	fmt.Fprintln(w, form)
 }
