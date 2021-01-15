5845d62d7ba304cd88c00052d52ee596585e9495
petros <pit.trak@gmail.com>
2021-01-15 17:33:08 +0200

    Writing a remember token hasher

--- exp/main.go
+++ exp/main.go
@@ -1,74 +1,20 @@
 package main
 
 import (
+	"DarkRoom/hash"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "root"
-	dbname   = "darkroom_dev"
-)
-
-type User struct {
-	gorm.Model
-	Name   string
-	Email  string `gorm:"not null;unique"`
-	Color  string
-	Orders []Order
-}
-
-type Order struct {
-	gorm.Model
-	UserID      uint
-	Amount      int
-	Description string
-}
 
 func main() {
-	// %s for string, %d for int
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := gorm.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+	toHash := []byte("this is my string to hash")
+	h := hmac.New(sha256.New, []byte("my-secret-key"))
+	h.Write(toHash)
+	b := h.Sum(nil)
+	fmt.Println(base64.URLEncoding.EncodeToString(b))
 
-	db.LogMode(true) // tracks the sql commands running behind the scenes
-	// db.DropTableIfExists(&User{})
-	db.AutoMigrate(&User{}, &Order{})
-
-	var u User
-	if err := db.Preload("Orders").First(&u).Error; err != nil {
-		panic(err)
-	}
-	createOrder(db, u, 1001, "Just a description #1")
-	createOrder(db, u, 999, "Just a description #2")
-	createOrder(db, u, 1000, "Just a description #3")
-	if err = db.Where("email = ?", "invalidEmail@gmail.com").First(&u).Error; err != nil {
-		switch err {
-		case gorm.ErrRecordNotFound:
-			fmt.Println("No user found!")
-		default:
-			panic(err)
-		}
-	}
-	fmt.Println(u)
-
-}
-
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	err := db.Create(&Order{
-		UserID:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	}).Error
-	if err != nil {
-		panic(err)
-	}
+	hmac := hash.NewHMAC("my-secret-key")
+	fmt.Println(hmac.Hash("this is my string to hash"))
 }
