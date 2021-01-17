b3056e2caa842e9c44b8db3fc10b10e092d8a4db
petrostrak <pit.trak@gmail.com>
2021-01-17 19:06:56 +0200

    Sharing a GORM connection and the GalleryService

--- models/galleries.go
+++ models/galleries.go
@@ -5,6 +5,16 @@ import "github.com/jinzhu/gorm"
 // Gallery is our image container resources that visitors view
 type Gallery struct {
 	gorm.Model
-	UserID uint `gorm:"not_null;index"`
-	Title string `gorm:"not_null"`
+	UserID uint   `gorm:"not_null;index"`
+	Title  string `gorm:"not_null"`
+}
+
+type GalleryService interface{}
+
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
 }
