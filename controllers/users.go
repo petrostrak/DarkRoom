d134db665189a6cd1b52a5eec4f6206cb57e11f0
petrostrak <pit.trak@gmail.com>
2021-01-17 15:03:15 +0200

    Dynamic alerts

--- controllers/users.go
+++ controllers/users.go
@@ -39,7 +39,15 @@ func NewUsers(us models.UserService) *Users {
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := u.NewView.Render(w, nil); err != nil {
+	type Alert struct {
+		Level string
+		Message string
+	}
+	a := Alert{
+		Level: "success",
+		Message: "Successfully render a dynamic alert!",
+	}
+	if err := u.NewView.Render(w, a); err != nil {
 		panic(err)
 	}
 }
