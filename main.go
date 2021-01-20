d1c7c5bb9fcc09ef0086ebb98e18a022aa4d39c6
petrostrak <pit.trak@gmail.com>
2021-01-20 18:28:17 +0200

    Serving static assets

--- main.go
+++ main.go
@@ -42,6 +42,11 @@ func main() {
 	r.Handle("/login", usersC.LoginView).Methods("GET")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
 
+	// Assets
+	assetsHandler := http.FileServer(http.Dir("./assets/"))
+	assetsHandler = http.StripPrefix("/assets/", assetsHandler)
+	r.PathPrefix("/assets/").Handler(assetsHandler)
+
 	// Image routes
 	imageHandler := http.FileServer(http.Dir("./images/"))
 	r.PathPrefix("/images/").Handler(http.StripPrefix("/images/", imageHandler))
